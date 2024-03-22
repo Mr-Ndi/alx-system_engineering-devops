2da100b0a46068c68ad91415102a0fd957643baa
ndiramiye2 <onlythenotes@gmail.com>
2024-03-22 20:48:22 +0200

    Nitafanya hivyo peke yangu

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Using Puppet, create a file in /tmp.
 file { '/tmp/school':
-ensure => present,
-content => "I love Puppet",
-owner => 'www-data',
-group => 'www-data',
-mode => '0744',
+ensure  => present,
+content => 'I love Puppet',
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0744',
 }
