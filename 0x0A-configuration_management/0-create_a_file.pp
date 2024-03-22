a88fdd63f2b8d75073b24c838a95100eecc88dcf
ndiramiye2 <onlythenotes@gmail.com>
2024-03-22 20:38:56 +0200

    Nitafanya hivyo peke yangu

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file { '/tmp/school'
-ensure=>'present',
-content=>"I love Puppet",
-owner=>'www-data',
-group=>'www-data',
-mode=>'0744',
+file { '/tmp/school':
+ensure => present,
+content => "I love Puppet",
+owner => 'www-data',
+group => 'www-data',
+mode => '0744',
 }
