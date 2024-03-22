115558b56919d14bf142a5a962b46ffcd7a132cf
ndiramiye2 <onlythenotes@gmail.com>
2024-03-22 21:52:58 +0200

    Nitafanya hivyo peke yangu

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # Using Puppet, create a manifest that kills a process
 exec {'proc_killer':
-command     => 'pkill killmenow',
-path        => '/usr/bin:/sbin',
-refreshonly => true'
+command => 'pkill killmenow',
+path    => '/usr/bin',
 }
