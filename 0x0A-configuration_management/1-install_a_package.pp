4cabb537ba4083732f29715d3f87e8307414d11b
ndiramiye2 <onlythenotes@gmail.com>
2024-03-23 10:06:29 +0200

    finally

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Puppet manifest to install flask
-package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+# Puppet manifest to install Flask version 2.1.0 using pip3
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => 'pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin', # Specify the path where pip3 is located
+  require => Package['python3-pip'], # Ensure python3-pip is installed first
 }
