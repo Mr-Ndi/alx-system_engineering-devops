7ff4783c1922bcb8c8d7f3051c401ccb018dfe6e
ndiramiye2 <onlythenotes@gmail.com>
2024-05-07 21:17:00 +0200

    Task zero

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url)
-    if response.status_code == 404:
-        return 0
-    else:
+    """ A function to deliver the nbr of subs to the console"""
+    header = {"User-Agent": "mr-ndi/1.0"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url,allow_redirects=False, headers=headers)
+    if reponse.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
+    else:
+        return (0)
