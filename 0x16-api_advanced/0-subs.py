66dc93c9d7e695c41b5ec40ce8420a659c8713df
ndiramiye2 <onlythenotes@gmail.com>
2024-05-07 21:20:28 +0200

    Nitafanya hivyo peke yangu

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ A function to deliver the nbr of subs to the console"""
-    header = {"User-Agent": "mr-ndi/1.0"}
+    headers = {"User-Agent": "mr-ndi/1.0"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url,allow_redirects=False, headers=headers)
-    if reponse.status_code == 200:
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
     else:
