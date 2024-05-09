3e1473f9e6fbd5fdc04483411bb7ccb3eabea4a0
ndiramiye2 <onlythenotes@gmail.com>
2024-05-09 22:42:42 +0200

    Nitafanya hivyo peke yangu

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,13 @@
-exec { 'fix-wordpress':
-  command => 'chmod 644 /var/www/html/wp-config.php',
-  path    => '/bin:/usr/bin:/sbin:/usr/sbin',
-  onlyif  => 'test $(stat -c "%a" /var/www/html/wp-config.php) -ne 644',
+# 0-strace_is_your_friend.pp
+
+# Make sure Apache service is running
+service { 'apache2':
+  ensure => 'running',
 }
 
-service { 'apache2':
-  ensure => running,
-  enable => true,
+# Use strace to find the issue and fix it
+#
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
