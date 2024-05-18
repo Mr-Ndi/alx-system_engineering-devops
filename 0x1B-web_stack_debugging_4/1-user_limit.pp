d78589a60361c10d8020774c5060a440b5c4c865
ndiramiye2 <onlythenotes@gmail.com>
2024-05-19 01:34:55 +0200

    Nitafanya hivyo peke yangu

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,13 @@
-# Increase the open file limit for the 'holberton' user
-exec { 'increase-open-files-limit-for-holberton':
-  command => 'ulimit -n 4096 && su - holberton',
-  path    => '/bin:/usr/bin',
-  user    => 'root',
+# Enable the user holberton to login and open files without error.
+
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
